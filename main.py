29ca2ec330e923768072ad33a36103bf81cd9786
bartoszkominko <bartoszkominko@gmail.com>
2021-09-03 18:34:25 +0200

    test

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import subprocess
 chia_directory = '/home/keysys/chia-blockchain/'
 print(os.getcwd())
 #os.chdir(chia_directory)
-subprocess.check_call('. ./activate', cwd=chia_directory)
-subprocess.check_call('chia plotnft show', cwd=chia_directory)
+subprocess.check_call('. ./activate'.split(), cwd=chia_directory)
+subprocess.check_call('chia plotnft show'.split(), cwd=chia_directory)
