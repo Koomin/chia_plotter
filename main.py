2509a38aa381d30a9e5dae81d1f6e411b16c710b
bartoszkominko <bartoszkominko@gmail.com>
2021-09-04 23:41:13 +0200

    Trying to join threads.

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import subprocess
 import json
 import logging
 import sys
-import threading
 import shutil
+import uuid
+import concurrent.futures
+import time
+from datetime import timedelta
 
-CHIA_FILE_SIZES = {
+PLOT_SIZE = {
     "25": 0.6,
     "32": 101.4,
     "33": 208.8,
@@ -13,64 +16,71 @@ CHIA_FILE_SIZES = {
     "35": 884.1
 }
 
+DISKS_FREE_SPACE = {
+
+}
+
+logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
+
 try:
     with open('config.json') as config:
         config_data = json.load(config)
         chia_directory = config_data['chia_directory']
         k_size = config_data['k_size']
         temp_path = config_data['temp_directory']
-        directory_path = config_data['final_directory']
+        final_directories = config_data['list_of_final_directories']
         plotting_address = config_data['plotting_address']
         threads = int(config_data["threads"])
         logging.info('Config loaded correctly.')
 except IOError as e:
-    logging.error(f'Cannot find config file.')
+    logging.error(f'Cannot find config file - interrupting.')
     sys.exit()
 except ValueError:
-    logging.error(f'Wrong number of threads provided.')
+    logging.error(f'Wrong number of threads provided - interrupting.')
     sys.exit()
 except KeyError:
-    logging.error(f'You didn\'t provide all config data.')
+    logging.error(f'You didn\'t provide all config data - interrupting.')
     sys.exit()
-command = f'chia plots create -k {k_size} -t {temp_path} -d {directory_path} -c {plotting_address}'
 
 
-class PlottingThread(threading.Thread):
-    def __init__(self, threadID):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-
-    def run(self):
-        logging.info(f'Starting {self.threadID} plotter.')
-        run_plotting(command)
-        logging.info(f'Exit {self.threadID} plotter.')
-
-
-def run_plotting(cmd):
+def run_plotting(k, directories):
+    thread_id = uuid.uuid4()
+    start = time.time()
+    logging.info(f'Starting plotter with ID {thread_id}.')
+    directory_path = enough_free_space(k, directories)
+    cmd = f'chia plots create -k {k} -t {temp_path} -d {directory_path} -c {plotting_address}'
     try:
         subprocess.check_call(cmd.split(), cwd=chia_directory)
     except IOError:
         logging.error('Chia directory wrong provided.')
         sys.exit()
+    end = time.time()
+    execution_time = timedelta(seconds=end - start)
+    logging.info(f'Exit plotter with ID {thread_id} - execution time {execution_time}.')
 
 
-def check_disk_space(path):
-    total, used, free = shutil.disk_usage(path)
-    free = free // (2 ** 30)
-    return free
+def check_disk_space(directories_list):
+    for directory in directories_list:
+        try:
+            total, used, free = shutil.disk_usage(directory)
+            free = free // (2 ** 30)
+            DISKS_FREE_SPACE[directory] = free
+        except FileNotFoundError:
+            logging.error(f'Cannot find directory : {directory}')
+            continue
 
 
-def enough_free_space(number_of_threads, k, path):
-    free_space = check_disk_space(path)
-    if number_of_threads * CHIA_FILE_SIZES[k] < free_space:
-        return True
-    return False
+def enough_free_space(k, directories):
+    for path in directories:
+        if DISKS_FREE_SPACE.get(path) and PLOT_SIZE[k] * 1.05 < DISKS_FREE_SPACE.get(path):
+            DISKS_FREE_SPACE[path] = DISKS_FREE_SPACE[path] - PLOT_SIZE[k]
+            return path
+    logging.error('Not enough disk space - interrupting.')
+    sys.exit()
 
 
-while enough_free_space(threads, k_size, directory_path):
-    for idx in range(0, threads):
-        PlottingThread(idx).start()
-    while threading.activeCount() > 0:
-        logging.info(f'{threading.activeCount()} plotters run.')
-
-logging.warning('Not enough disk space.')
+if __name__ == "__main__":
+    while True:
+        check_disk_space(final_directories)
+        with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+            executor.map(run_plotting(k_size, final_directories), range(threads))
